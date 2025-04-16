c1d4ce4ea1a2559ea84a230587ea7eeea0123a8f
Erik Jigvall <erijig@gmail.com>
2025-04-16 19:30:20 +0200

    Initial commit

---
+++ Source/ProjectSpook/ProjectSpookGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSpookGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectSpookGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectSpookGameMode();
+};
+
+
+
